d92a4f1b61d82e2acb614720148a2a905c6ea459
A Gom <arniethepie@gmail.com>
2023-07-31 18:36:13 +0000

    less verbose get_atv_proxy.sh

--- get_atv_proxy.sh
+++ get_atv_proxy.sh
@@ -1,9 +1,6 @@
-echo "Connect to all atvs currently online"
 while read -r p; do
-  adb connect ${p}:5555 
-  echo "Checking proxy settings"
-  adb shell -n settings get global http_proxy
+  adb connect ${p}:5555 > /dev/null
+  { echo "${p}: "; adb shell -n settings get global http_proxy;} 
   adb disconnect ${p} > /dev/null
-  sleep 5
 done < atvips
 
